9acd762853e0fab6337c89a8eedbccae35c1b8a2
Clayton Carter <claytonrcarter@gmail.com>
2024-12-02 20:45:36 -0500

    feat(lsp): download lsp from releases

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,121 @@
-use zed_extension_api as zed;
+use std::fs;
+use zed::LanguageServerId;
+use zed_extension_api::{self as zed, Result};
 
-struct LanguageLedger {
-    // ... state
+struct LedgerExtension {
+    cached_binary_path: Option<String>,
 }
 
-impl zed::Extension for LanguageLedger {
+impl LedgerExtension {
+    fn language_server_binary_path(
+        &mut self,
+        language_server_id: &LanguageServerId,
+    ) -> Result<String> {
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(path.clone());
+            }
+        }
+
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = zed::latest_github_release(
+            "claytonrcarter/ledger-language-server",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: true,
+            },
+        )?;
+
+        let version = release.version.replace("v", "");
+        let (platform, arch) = zed::current_platform();
+
+        let asset_name = format!(
+            "ledger-language-server-{version}-{os}-{arch}",
+            os = match platform {
+                zed::Os::Mac => "apple",
+                zed::Os::Linux => "linux",
+                zed::Os::Windows =>
+                    return Err(
+                        "unsupported os: ledger-language-server is not released for windows"
+                            .to_string()
+                    ),
+            },
+            arch = match arch {
+                zed::Architecture::Aarch64 => "aarch64",
+                zed::Architecture::X8664 => "x86_64",
+                zed::Architecture::X86 =>
+                    return Err(
+                        "unsupported arch: ledger-language-server is not released for x86"
+                            .to_string()
+                    ),
+            },
+        );
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
+
+        let version_dir = format!("ledger-language-server-{}", release.version);
+        fs::create_dir_all(&version_dir)
+            .map_err(|err| format!("failed to create directory '{version_dir}': {err}"))?;
+
+        if !fs::metadata(&asset_name).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+
+            zed::download_file(
+                &asset.download_url,
+                &asset_name,
+                zed::DownloadedFileType::Uncompressed,
+            )
+            .map_err(|err| format!("failed to download file: {err}"))?;
+
+            zed::make_file_executable(&asset_name)?;
+
+            let entries = fs::read_dir(".")
+                .map_err(|err| format!("failed to list working directory {err}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|err| format!("failed to load directory entry {err}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(asset_name.clone());
+        Ok(asset_name)
+    }
+}
+
+impl zed::Extension for LedgerExtension {
     fn new() -> Self
     where
         Self: Sized,
     {
-        LanguageLedger {}
+        LedgerExtension {
+            cached_binary_path: None,
+        }
     }
 
     fn language_server_command(
         &mut self,
-        _language_server_id: &zed::LanguageServerId,
+        language_server_id: &zed::LanguageServerId,
         _worktree: &zed::Worktree,
     ) -> zed::Result<zed::Command> {
         Ok(zed::Command {
-            // FIXME
-            command: "/Users/crcarter/src/ledger-lsp/target/debug/ledger-lsp".to_owned(),
-            // command: "/Users/crcarter/src/ledger-lsp/target/release/ledger-lsp".to_owned(),
-            args: Vec::new(),
+            command: self.language_server_binary_path(language_server_id)?,
+            // command: "/Users/crcarter/src/ledger-language-server/target/debug/ledger-lsp"
+            //     .to_owned(),
+            args: vec!["lsp".to_string()],
             env: Vec::new(),
         })
     }
 }
 
-zed::register_extension!(LanguageLedger);
+zed::register_extension!(LedgerExtension);
